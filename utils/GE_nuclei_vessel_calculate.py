f4730f022556a2270f7368aa1a1e009ec5730ed6
Yingnan Ju <bunny_gg@hotmail.com>
2020-08-18 02:17:39 -0400

    added vis for GE nuclei mapping to vessel

---
+++ utils/GE_nuclei_vessel_calculate.py
@@ -0,0 +1,158 @@
+from skimage import io
+import math
+import csv
+import os
+
+
+def write_csv(path, list_of_columns, list_of_names=None):
+    with open(path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        if list_of_names is not None:
+            writer.writerow(list_of_names)
+        lines = []
+        for _i in range(len(list_of_columns[0])):
+            lines.append([column[_i] for column in list_of_columns])
+        writer.writerows(lines)
+
+
+def read_csv(path):
+    with open(path, 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        _headers = next(reader, None)
+
+        # get column
+        _columns = {}
+        for h in _headers:
+            _columns[h] = []
+        for row in reader:
+            for h, v in zip(_headers, row):
+                _columns[h].append(v)
+
+        for item in _headers:
+            print(item)
+
+        return _headers, _columns
+
+
+# prs = Presentation(r'C:\Users\bunny\Desktop\KidneyAnnotated-GW.pptx')
+# print(prs.slide_height, prs.slide_width)
+# slide = prs.slides[0]
+#
+# shapes = slide.shapes
+#
+# print(len(shapes))
+#
+# unit_per_um = 1024646 / 50
+# unit_per_pixel = 28346400 / 2232
+# pixel_per_um = unit_per_um / unit_per_pixel
+#
+nuclei_id_list = list()
+nuclei_x_list = list()
+nuclei_y_list = list()
+nuclei_distance_list = list()
+nuclei_nearest_vessel_x_list = list()
+nuclei_nearest_vessel_y_list = list()
+
+root_path = r'C:\Users\bunny\Desktop\Region_6'
+nuclei_file_name = 'quant_slide77_region6.csv'
+vessel_file_name = 'vessel.csv'
+nuclei_file_path = os.path.join(root_path, nuclei_file_name)
+
+n_headers, n_columns = read_csv(nuclei_file_path)
+for i in range(0, 3):  # len(n_headers)):
+    n_columns[n_headers[i]] = [float(value) for value in n_columns[n_headers[i]]]
+n_columns['Y'] = [float(value) for value in n_columns['Y']]
+
+nuclei_x_list = [float(value / 2) for value in n_columns['Y'][::2]]
+nuclei_y_list = [float(value / 2) for value in n_columns['X'][::2]]
+nuclei_id_list = [int(value) for value in n_columns['Cell ID'][::2]]
+
+# nuclei_image = io.imread('../images/nuclei_ml.png')  # [::-1, :]
+# _nid = 0
+# for i in range(len(nuclei_image)):
+#     row = nuclei_image[i]
+#     for j in range(len(row)):
+#         pixel = row[j]
+#         if pixel[0] > 200:
+#             # each nuclear is 2x2 pixels
+#             # get the average (+0.5)
+#             # and erase other 3 points
+#             nuclei_y_list.append((i + 0.5) / pixel_per_um)
+#             nuclei_x_list.append((j + 0.5) / pixel_per_um)
+#             nuclei_image[i][j + 1] *= 0
+#             nuclei_image[i + 1][j] *= 0
+#             nuclei_image[i + 1][j + 1] *= 0
+#             nuclei_id_list.append(_nid)
+#             _nid += 1
+print(len(nuclei_x_list))
+
+vessel_x_list = list()
+vessel_y_list = list()
+
+vessel_image = io.imread(r'C:\Users\bunny\Desktop\Region_6\Vessels_S77R6_mask_05.png')  # [::-1, :]
+for i in range(len(vessel_image)):
+    row = vessel_image[i]
+    for j in range(len(row)):
+        pixel = row[j]
+        if pixel[0] > 200:
+            vessel_y_list.append(i)
+            vessel_x_list.append(j)
+print(len(vessel_x_list))
+
+write_csv('../csv/nuclei_GE.csv',
+          [nuclei_id_list,
+           nuclei_x_list,
+           nuclei_y_list],
+          ['id', 'x', 'y'])
+
+write_csv('../csv/vessel_GE.csv',
+          [vessel_x_list, vessel_y_list],
+          ['x', 'y'])
+
+for nid in range(len(nuclei_id_list)):
+    _min_dist = 600
+    _min_vessel_x = 0
+    _min_vessel_y = 0
+    _nx = nuclei_x_list[nid]
+    _ny = nuclei_y_list[nid]
+    _has_near = False
+    for v in range(len(vessel_x_list)):
+        _vx = vessel_x_list[v]
+        _vy = vessel_y_list[v]
+        if abs(_nx - _vx) < _min_dist and abs(_ny - _vy) < _min_dist:
+            _dist = math.sqrt((_nx - _vx) ** 2 + (_ny - _vy) ** 2)
+            if _dist < _min_dist:
+                _has_near = True
+                _min_dist = _dist
+                _min_vessel_x = _vx
+                _min_vessel_y = _vy
+    if not _has_near:
+        print("NO NEAR")
+    nuclei_distance_list.append(_min_dist)
+    nuclei_nearest_vessel_x_list.append(_min_vessel_x)
+    nuclei_nearest_vessel_y_list.append(_min_vessel_y)
+    if nid % 100 == 0:
+        print('\r' + str(nid), end='')
+
+print(len(nuclei_id_list), len(nuclei_x_list), len(nuclei_y_list), len(nuclei_distance_list),
+      len(nuclei_nearest_vessel_x_list), len(nuclei_nearest_vessel_y_list))
+
+write_csv('../csv/nuclei_GE.csv',
+          [nuclei_id_list,
+           nuclei_x_list,
+           nuclei_y_list,
+           nuclei_distance_list,
+           nuclei_nearest_vessel_x_list,
+           nuclei_nearest_vessel_y_list],
+          ['id', 'x', 'y', 'distance', 'vx', 'vy'])
+
+write_csv('../csv/vessel_GE.csv',
+          [vessel_x_list, vessel_y_list],
+          ['x', 'y'])
+
+import matplotlib.pyplot as plt
+
+# plt.scatter(vessel_x_list, vessel_y_list, c='r')
+# plt.scatter(nuclei_x_list, nuclei_y_list, c='b')
+plt.hist(nuclei_distance_list, bins=100)
+plt.show()
--- visualization/distance_bokeh.py
+++ visualization/distance_bokeh.py
@@ -9,7 +9,7 @@ from bokeh.models import HoverTool
 
 def read_csv(path):
     with open(path, 'r', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile, delimiter='\t')
+        reader = csv.reader(csvfile, delimiter=',')
         _headers = next(reader, None)
 
         # get column
@@ -26,70 +26,77 @@ def read_csv(path):
         return _headers, _columns
 
 
-tools_list = "pan," \
-             "box_select," \
-             "lasso_select," \
-             "box_zoom, " \
-             "wheel_zoom," \
-             "reset," \
-             "save," \
-             "help"
-# "hover," \
+if __name__ == '__main__':
+    tools_list = "pan," \
+                 "box_select," \
+                 "lasso_select," \
+                 "box_zoom, " \
+                 "wheel_zoom," \
+                 "reset," \
+                 "save," \
+                 "help"
+    # "hover," \
 
-root_path = r'../csv'
-nuclei_file_name = 'nuclei.csv'
-vessel_file_name = 'vessel.csv'
-nuclei_file_path = os.path.join(root_path, nuclei_file_name)
-vessel_file_path = os.path.join(root_path, vessel_file_name)
+    root_path = r'../csv'
+    nuclei_file_name = 'nuclei_GE.csv'
+    vessel_file_name = 'vessel_GE.csv'
+    nuclei_file_path = os.path.join(root_path, nuclei_file_name)
+    vessel_file_path = os.path.join(root_path, vessel_file_name)
 
-output_file("result/distance.html")
+    output_file("result/distance.html")
 
-drug_color = OrderedDict([
-    ("Item Category", "#0d33ff"),
-    ("Item metadata", "#c64737"),
-    ("User profile", "black"),
-])
+    drug_color = OrderedDict([
+        ("Item Category", "#0d33ff"),
+        ("Item metadata", "#c64737"),
+        ("User profile", "black"),
+    ])
 
-gram_color = OrderedDict([
-    ("negative", "#e69584"),
-    ("positive", "#aeaeb8"),
-])
+    gram_color = OrderedDict([
+        ("negative", "#e69584"),
+        ("positive", "#aeaeb8"),
+    ])
 
-p = figure(plot_width=1000, plot_height=1000, title='nuclei/vessel distance', match_aspect=True,
-           tools=tools_list,
+    p = figure(plot_width=3727, plot_height=2348, title='nuclei/vessel distance', match_aspect=True,
+               tools=tools_list,
 
-           )
+               )
 
-v_headers, v_columns = read_csv(vessel_file_path)
-for i in range(0, len(v_headers)):
-    v_columns[v_headers[i]] = [float(value) for value in v_columns[v_headers[i]]]
-v_columns['y'] = [(1400 - float(value)) for value in v_columns['y']]
+    p.xgrid.visible = False
+    p.ygrid.visible = False
+    # p.axis.visible = False
+    p.background_fill_alpha = 0.0
+    # p.outline_line_color = None
 
-data = dict()
-for header in v_headers:
-    data[header] = v_columns[header]
-v_df = pd.DataFrame(data)
+    v_headers, v_columns = read_csv(vessel_file_path)
+    for i in range(0, len(v_headers)):
+        v_columns[v_headers[i]] = [float(value) for value in v_columns[v_headers[i]]]
+    v_columns['y'] = [float(value) for value in v_columns['y']]
 
-p.circle(x='x', y='y', source=v_df, color='orange', size=1, alpha=0.1)
+    data = dict()
+    for header in v_headers:
+        data[header] = v_columns[header]
+    v_df = pd.DataFrame(data)
 
-n_headers, n_columns = read_csv(nuclei_file_path)
-for i in range(1, len(n_headers)):
-    n_columns[n_headers[i]] = [float(value) for value in n_columns[n_headers[i]]]
-n_columns['y'] = [(1400 - float(value)) for value in n_columns['y']]
-n_columns['vy'] = [(1400 - float(value)) for value in n_columns['vy']]
-n_columns['alpha'] = [(float(value) + 10) / 100 for value in n_columns['distance']]
+    p.circle(x='x', y='y', source=v_df, color='orange', size=4, alpha=0.4)
 
-data = dict()
-for header in n_headers:
-    data[header] = n_columns[header]
-    data['alpha'] = n_columns['alpha']
-n_df = pd.DataFrame(data)
+    n_headers, n_columns = read_csv(nuclei_file_path)
+    for i in range(1, len(n_headers)):
+        n_columns[n_headers[i]] = [float(value) for value in n_columns[n_headers[i]]]
+    n_columns['y'] = [float(value) for value in n_columns['y']]
+    n_columns['vy'] = [float(value) for value in n_columns['vy']]
+    n_columns['alpha'] = [(float(value) + 200) / 800 for value in n_columns['distance']]
 
-p.segment(x0='x', y0='y', x1='vx', source=n_df, y1='vy', color="#CAB2D6", line_width=1)
-circle = p.circle(x='x', y='y', source=n_df, color='blue', alpha='alpha', size=3)
-g1_hover = HoverTool(renderers=[circle], tooltips=[('X', "@x"), ('Y', "@y"), ('distance', "@distance")])
-p.add_tools(g1_hover)
+    data = dict()
+    for header in n_headers:
+        data[header] = n_columns[header]
+        data['alpha'] = n_columns['alpha']
+    n_df = pd.DataFrame(data)
 
-# p.legend.location = "bottom_right"
+    p.segment(x0='x', y0='y', x1='vx', source=n_df, y1='vy', color="#CAB2D6", line_width=2)
+    circle = p.circle(x='x', y='y', source=n_df, color='blue', alpha='alpha', size=4)
+    g1_hover = HoverTool(renderers=[circle], tooltips=[('X', "@x"), ('Y', "@y"), ('distance', "@distance")])
+    p.add_tools(g1_hover)
 
-show(p)
+    # p.legend.location = "bottom_right"
+
+    show(p)
