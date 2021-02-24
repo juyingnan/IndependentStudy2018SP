8ccc6692f1a5dc2cba509379873319bfc10a2013
juyingnan <bunny_gg@hotmail.com>
2021-02-24 02:54:41 -0500

    integrate the color code with original viz

--- visualization/kidney_distance_bokeh.py
+++ visualization/kidney_distance_bokeh.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     vessel_output_name = 'vessel.csv'
     vessel_file_path = os.path.join(output_root_path, vessel_output_name)
 
-    output_file(os.path.join(output_root_path, 'distance.html'))
+    output_file(os.path.join(output_root_path, f'{input_id}.html'))
 
     p = figure(match_aspect=True,
                plot_width=int(2000 * 1.075), plot_height=int(2000 * 1.075),
@@ -71,7 +71,7 @@ if __name__ == '__main__':
         data[header] = v_columns[header]
     v_df = pd.DataFrame(data)
 
-    p.circle(x='x', y='y', source=v_df, color='#91672c', size=4, alpha=1)
+    # p.circle(x='x', y='y', source=v_df, color='#91672c', size=6, alpha=1)
 
     n_headers, n_columns = read_csv(nuclei_file_path)
     # for i in range(1, len(n_headers)):
@@ -83,6 +83,29 @@ if __name__ == '__main__':
     n_columns['alpha'] = [(float(value) + 500) / 1500 for value in n_columns['distance']]
     n_columns['color'] = ['#%02x%02x%02x' % (0, int(255 * float(value) / 1000), int(255 * (1000 - float(value)) / 1000))
                           for value in n_columns['distance']]
+
+    cell_type_pool = [
+        'Ascending Thin Limb to Thick Ascending Limb Cell',
+        'Ascending Thin Limb Cell',
+        'Proximal Tubule Cell',
+        'Fibroblast',
+        'Thick Ascending Limb Cell',
+        'Macrophage',
+        'Distal Convoluted Tubule Cell',
+        'Myofibroblast',
+        'Connecting Tubule',
+        'Principal Cell',
+        'Descending Thin Limb Cell',
+        'placeholder_0',
+        'Podocyte',
+        'placeholder_1',
+        'Endothelial Cell',
+        'placeholder_2',
+        'Vascular Smooth Muscle Cell and Pericyte'
+        'placeholder_3',
+        'Intercalated Alpha Cell',
+        'Intercalated Beta Cell',
+    ]
 
     data = dict()
     for header in n_headers:
@@ -105,14 +128,14 @@ if __name__ == '__main__':
         p.scatter(x='x', y='y', source=n_df, fill_alpha=0.8, size=6,
                   # marker=factor_mark('style_label', markers, roman_label),
                   marker='circle',
-                  color=factor_cmap('type', 'Category20_16', cell_types),
+                  color=factor_cmap('type', 'Category20_20', cell_type_pool),
                   # muted_color=factor_cmap(label['real_label_list'], 'Category10_8',
                   #                         label['standard_label_list']),
                   muted_alpha=0.1, view=view,
                   legend_label=legend_label)
         p.segment(x0='x', y0='y', x1='vx', source=n_df, y1='vy',
                   # color="red",
-                  color=factor_cmap('type', 'Category20_16', cell_types),
+                  color=factor_cmap('type', 'Category20_20', cell_type_pool),
                   alpha=0.2, line_width=1,
                   view=view,
                   legend_label=legend_label
@@ -122,5 +145,5 @@ if __name__ == '__main__':
     # p.add_tools(g1_hover)
 
     p.legend.location = "bottom_right"
-    p.legend.click_policy = "hide"
+    p.legend.click_policy = "mute"
     show(p)
